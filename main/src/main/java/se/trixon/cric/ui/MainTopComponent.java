f79976bcfc8da9a9cc1c96add42e1932124d7ea0
Patrik Karlström <patrik@trixon.se>
2024-03-02 19:58:53 +0100

    Add empty task window

---
+++ main/src/main/java/se/trixon/cric/ui/MainTopComponent.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 Patrik Karlström <patrik@trixon.se>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.cric.ui;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import org.netbeans.api.settings.ConvertAsProperties;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.util.NbBundle.Messages;
+import org.openide.windows.TopComponent;
+import se.trixon.almond.nbp.fx.FxTopComponent;
+
+/**
+ * Top component which displays something.
+ */
+@ConvertAsProperties(
+        dtd = "-//se.trixon.cric.ui//Main//EN",
+        autostore = false
+)
+@TopComponent.Description(
+        preferredID = "MainTopComponent",
+        //iconBase="SET/PATH/TO/ICON/HERE",
+        persistenceType = TopComponent.PERSISTENCE_ONLY_OPENED
+)
+@TopComponent.Registration(mode = "explorer", openAtStartup = true)
+@ActionID(category = "Window", id = "se.trixon.cric.ui.MainTopComponent")
+@ActionReference(path = "Menu/Window" /*, position = 333 */)
+@TopComponent.OpenActionRegistration(
+        displayName = "#CTL_MainTopComponent",
+        preferredID = "MainTopComponent"
+)
+@Messages({
+    "CTL_MainTopComponent=Tasks"
+})
+public final class MainTopComponent extends FxTopComponent {
+
+    public MainTopComponent() {
+        setName(Bundle.CTL_MainTopComponent());
+    }
+
+    @Override
+    protected void initFX() {
+//        var taskListEditor = new TaskListEditor();
+//        setScene(new Scene(taskListEditor.getEditableList()));
+        setScene(new Scene(new Label("X")));
+    }
+
+    void writeProperties(java.util.Properties p) {
+        // better to version settings since initial version as advocated at
+        // http://wiki.apidesign.org/wiki/PropertyFiles
+        p.setProperty("version", "1.0");
+        // TODO store your settings
+    }
+
+    void readProperties(java.util.Properties p) {
+        String version = p.getProperty("version");
+        // TODO read your settings according to their version
+    }
+}
